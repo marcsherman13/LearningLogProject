f6d917ffeca09c94a5696bf1e1e6f466ca9e67e0
Marc Sherman <marcalansherman@gmail.com>
2026-04-15 21:38:50 -0500

    ready for render deployment

--- AdvPython/LearningLogProject/learning_log/settings.py
+++ AdvPython/LearningLogProject/learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9djbp5)%4%1a0*lebvcqcpv$v*#_*e_vwdb1h!k&8e*k&ac^p&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
